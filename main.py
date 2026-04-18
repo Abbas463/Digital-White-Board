e9c9daa63fdd2b6980ece09a98764f8e0c9514b1
Abbas463 <abbashilal463@gmail.com>
2026-04-18 10:01:48 +0430

    Simple UI created

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from tkinter.colorchooser import askcolor
 from tkinter import ttk
 from tkinter import dialog
 import os
+
+
+win = Tk()
+win.title('Digital White Board')
+win.geometry('1050x570+150+50')
+win.mainloop()
